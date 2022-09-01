d6bd6cacd27d868b149353026dd14b04496f7b40
Walter <walter@MacBook-Pro-de-Walter.local>
2022-09-01 18:20:51 -0500

    fix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const countdown = (deadline, elem, finalmessage) => {
   }, 1000)
 }
 
-countdown('Sep 3 2022 00:00:00 GMT-0500', 'clock', '¿Hola bichita que haces?');
+countdown('Sep 03 2022 00:00:00 GMT-0500', 'clock', '¿Hola amor que haces?');
 
 
 window.onload = function() {
