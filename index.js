683b340df472ce73b0c76799d8fbad756b5a58af
Walter <walter@MacBook-Pro-de-Walter.local>
2022-09-01 21:50:37 -0500

    fix: edit styles in resposive

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const getRemainTime = deadline => {
+const getRemainTime = (deadline) => {
   let now = new Date(),
     remainTime = (new Date(deadline) - now + 1000) / 1000, // se le suma un segundo porque es un tiempo de demora
-    remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
-    remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
-    remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
-    remainDays = Math.floor(remainTime / (3600 * 24))
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
+    remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+    remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
 
   return {
     remainTime,
     remainMinutes,
     remainHours,
     remainDays,
-    remainSeconds
-  }
+    remainSeconds,
+  };
 };
 
 const countdown = (deadline, elem, finalmessage) => {
   const el = document.getElementById(elem);
-  const mensajeNuevo = document.getElementById('card');
-  const gridMensaje = document.getElementById('grid')
+  const mensajeNuevo = document.getElementById("card");
+  const gridMensaje = document.getElementById("grid");
   const timerUpdate = setInterval(() => {
     let t = getRemainTime(deadline);
     el.innerHTML = `💝${t.remainDays}Días:${t.remainHours}h:${t.remainMinutes}m:${t.remainSeconds}s💝`;
@@ -26,20 +26,18 @@ const countdown = (deadline, elem, finalmessage) => {
     if (t.remainTime <= 1) {
       clearInterval(timerUpdate);
       el.innerHTML = finalmessage;
-      gridMensaje.style.display = 'none';
-      mensajeNuevo.style.display = 'block';
-
-    }else {
-      gridMensaje.style.display = "grid"
+      gridMensaje.style.display = "none";
+      mensajeNuevo.style.display = "block";
+    } else {
+      gridMensaje.style.display = "grid";
     }
-  }, 1000)
-}
+  }, 1000);
+};
 
-countdown('Sep 03 2022 00:00:00 GMT-0500', 'clock', '¿Hola amor que haces?');
+countdown("Sep 03 2022 00:00:00 GMT-0500", "clock", "¿Hola amor que haces?");
 
-
-window.onload = function() {
+window.onload = function () {
   let content = document.getElementById("loader");
   content.style.visibility = "hidden";
-  content.style.opacity = "0"
-}
+  content.style.opacity = "0";
+};
